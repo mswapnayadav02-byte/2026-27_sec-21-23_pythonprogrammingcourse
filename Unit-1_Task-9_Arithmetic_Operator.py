38da8fb1b13ca1610e35941bdbc1d87b51aa795a
mswapnayadav02-byte <mswapnayadav02@gmail.com>
2026-09-08 13:23:36 +0530

    arithmetic_operator

---
+++ Unit-1_Task-9_Arithmetic_Operator.py
@@ -0,0 +1,16 @@
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+c=a+b #addition
+print(c)
+c=a-b #subtraction
+print(c)
+c=a*b #multiplication
+print(c)
+c=a/b #division
+print(c)
+c=a%b #modulus
+print(c)
+c=a**b #exponentiation
+print(c)
+c=a//b #floor division
+print(c)
