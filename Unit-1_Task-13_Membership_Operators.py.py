208e9180937a7ff22261128ee89d3d36fa4477ad
mswapnayadav02-byte <mswapnayadav02@gmail.com>
2026-09-18 15:56:52 +0530

    membership_operators

---
+++ Unit-1_Task-13_Membership_Operators.py.py
@@ -0,0 +1,10 @@
+b='a' in 'apple'
+print("result of b:" , b)
+b='a' not in 'apple'
+print("result of b:" , b)
+b='z' in 'apple'
+print("result of b:" , b)
+b='z' not in 'apple'
+print("result of b:" , b)
+b='name 'in "my name 'is python"
+print("result of b:" , b)
