b5a5f19d7efe33348af3fa21bc13323b29e3738d
mswapnayadav02-byte <mswapnayadav02@gmail.com>
2026-09-18 15:56:20 +0530

    identify_operators

---
+++ Unit-1_Task-12_Identity_Operators.py.py
@@ -0,0 +1,17 @@
+a=[2,6,7]
+b=a
+c= a is b
+print("result of",a,"is",b,"is",c)
+
+a=[2,6,7]
+b=[2,6,7]
+c= a is b
+print("result of",a,"is",b,"is",c)
+a=[2,1,3]
+b=a
+c= a is b
+print("result of",a,"is",b,"is",c)
+a=[2,1,3]
+b=[2,1,3]
+c= a is b
+print("result of",a,"is",b,"is",c)
